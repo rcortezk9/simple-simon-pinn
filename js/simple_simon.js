4982b07ce2c3aac6eaaf367a64950e7ce8915924
rcortezk9 <rcortezk9@gmail.com>
2017-05-01 16:09:13 -0500

    added box to hold the four game's pads added display to show the level added styling to pads and display added special font

--- js/simple_simon.js
+++ js/simple_simon.js
@@ -3,6 +3,14 @@
  */
 $(document).ready(function () {
     'use strict'
+//    Starting Variables
 
+//    Audio files
+
+//    Functions for when the pads are press
+
+//    Function for random sequences
+
+//    Simon's turn.  Make sure there is like at least a one second wait between Simon's choices
 
 });
