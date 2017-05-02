3cfdeb5a67ccab635cb0ea896460ecc31eaf20d9
rcortezk9 <rcortezk9@gmail.com>
2017-05-02 14:11:16 -0500

    add function to select a random number put it in array for randomly selected color add function to interval for simon's presses add function that would compare the player's sequences to simon's sequence

--- js/simple_simon.js
+++ js/simple_simon.js
@@ -2,15 +2,15 @@
  * Created by renecortez on 5/1/17.
  */
 $(document).ready(function () {
-    'use strict'
+    'use strict';
 //    Starting Variables
     var simonSays = [];
     var playerSays = [];
     var simonSequence = "";
     var playerSequence = "";
-    var round = 1; // the 'level' of the game, max 20
+    var round = 1; // Starting 'level' of the game. A max of 20 rounds
     var press = 0; // the index of simonSays array, i.e. simon's pad presses
-    var mode = "safe" // safe and strict
+    var mode = "safe"; // safe and strict
 
 
 //    Audio files
@@ -54,21 +54,89 @@ $(document).ready(function () {
     }
 
 //    Populate Simon says
-    function simonSays(){
+    function simonSaysWhat(){
         simonSays = [];
-        for(i = 0; i < 20; i++){
+        for(var i = 0; i < 20; i++){
             simonSays.push(Math.floor(Math.random()*4)+1);
         }
     }
 
-//    Simon's turn.  Make sure there is like at least a one second wait between Simon's choices
-
+//    Simon's Play.  Make sure there is at least a one second wait between Simon's choices
+    function simonTurn() {
+        setTimeout(function() {
+            if (simonSays[press] === 1) {
+                greenPad();
+            } else if (simonSays[press] === 2) {
+                redPad();
+            } else if (simonSays[press] === 3) {
+                yellowPad();
+            } else if (simonSays[press] === 4) {
+                bluePad();
+            }
+            press++;
+            if (press < round) {
+                simonPlays();
+            }
+        }, 1500)
+    }
 //    Compare players sequence to simon's sequence match.
+    function comparedSequence() {
+        simonSequence = simonSays.slice(0, round).join("");
+        playerSequence = playerSays.join("");
+        if (simonSequence === playerSequence) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 
 //    Reset play
+    function playReset() {
+        press = 0;
+        playerSays = [];
+    }
 
 //    reset game
+    function gameReset() {
+        playReset();
+        round = 1;
+        $(".display").text("01");
+        simonSayWhat();
+    }
 
 //    Update display
+    function displayUpdate() {
+        if (round < 10) {
+            $(".display").text("0" + round);
+        } else {
+            $(".display").text(round);
+        }
+    }
+
+//    Determine when to the next round
+
+    function nextRound(){
+        if (round < 20){
+            if (playerSays === round){
+                if (comparedSequence()){
+                    round++;
+                    displayUpdate();
+                    playReset();
+                    simonTurn();
+                } else {
+                    if (mode === 'safe'){
+                        $('.display').text('!!');
+                        playReset();
+                        setTimeout(displayUpdate, 1000);
+                        setTimeout(simonTurn, 500);
+                    } else {
+                        $('.display').text('XX');
+
+                    }
+
+                }
+            }
+        }
+    }
 
 });
